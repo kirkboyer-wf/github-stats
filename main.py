34bd17da1454eb666cd54ca1891b348e11e5023c
Kirk Boyer <kirk.boyer@webfilings.com>
2014-08-14 13:23:20 -0600

    Added handlers and html templates for webpages

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 # import sys
 
 from google.appengine.ext import webapp
-
+from pages import IndexPage
+from pages import ReportPage
 # import settings
 
 app = webapp.WSGIApplication([
+    ('/', IndexPage),
+    ('/report', ReportPage)
     # ('/url/that/was/hit/', HandlerClass),
     # list of these!
 ], debug=True)
