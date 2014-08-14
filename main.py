4a1728ccf71a35601c4c493a5a7467a628d9747e
Kirk Boyer <kirk.boyer@webfilings.com>
2014-08-14 12:48:10 -0600

    started main app handler

---
+++ main.py
@@ -0,0 +1,11 @@
+# import os
+# import sys
+
+from google.appengine.ext import webapp
+
+# import settings
+
+app = webapp.WSGIApplication([
+    # ('/url/that/was/hit/', HandlerClass),
+    # list of these!
+], debug=True)
