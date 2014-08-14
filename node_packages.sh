45fc2816024c5cc3d655d7e87f0732feca5c73ac
Kirk Boyer <kirk.boyer@webfilings.com>
2014-08-14 11:22:34 -0600

    Previous commit did not actually delete files.

--- node_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Go to http://nodejs.org/ and install node.js and npm (which comes with node)
-
-npm install -g coffee-script
-npm install -g mocha
-npm install -g less
-npm install uglify-js
-npm install coffeelint
