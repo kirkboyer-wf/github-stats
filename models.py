2d5c91b249ca046f525035f0f64761908a592299
Kirk Boyer <kirk.boyer@webfilings.com>
2014-09-03 14:29:39 -0600

    Actually record language in comments

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ class Comment(ndb.Model):
     github_id = ndb.IntegerProperty()
     pull_request_id = ndb.IntegerProperty()
     repo = ndb.StringProperty()
+    language = ndb.StringProperty()
     author = ndb.StringProperty()
     body = ndb.TextProperty()
     users = ndb.ComputedProperty(_usernames_mentioned_in_comment)
