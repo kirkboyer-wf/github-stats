5a2dff36e0f5875eb50d15c24fe3459b0fc2817a
Kirk Boyer <kirk.boyer@webfilings.com>
2014-09-04 16:55:43 -0600

    removed outdated pages.py code

--- console/pages.py
+++ console/pages.py
@@ -22,41 +22,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 GIT_OBJECT = Github(login_or_token=settings.AUTH_TOKEN)
 
 
-def fill_notifications_data():
-    # TODO: check if we already have data.
-    if NotificationEntry.all().filter('timestamp >', datetime.datetime.now() +
-                                      datetime.timedelta(-1)):
-        logging.info("Already have stuff from within 1 days!!!")
-        # then we've already got data
-        return
-
-    notes = [NotificationEntry(user=note['user'],
-                               repo=note['repo'],
-                               timestamp=note['timestamp'],
-                               language=note['language'])
-             for note in get_all_notifications_from_org(
-                 GIT_OBJECT.get_organization(settings.ORG_NAME))]
-    # Store raw notifications information
-
-    user_languages = {}
-    for note in notes:
-        note.put()
-        # store by-user data
-        if note.user in user_languages:
-            user_languages[note.user][note.language] += 1
-        else:
-            user_languages[note.user] = {note.language: 1}
-
-    for user, languages in user_languages.items():
-        u = UserEntry(languages=languages,
-                      user=user)
-        u.put()
-
-
-def find_best_user_for_language(language):
-    users = UserEntry.all().fetch()
-
-
 class Page(webapp2.RequestHandler):
     def _make_page(self, page_name, template_values):
         template = JINJA_ENVIRONMENT.get_template(
@@ -74,11 +39,7 @@ class IndexPage(Page):
         pass
 
 
-class ReportPage(Page):
     def get(self):
-        fill_notifications_data()
-        template_values = {}
-        self._make_page('report', template_values)
 
     def post(self):
         pass
