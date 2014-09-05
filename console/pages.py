272dc5b40b4d79b7f107ece271109116f6c321c5
Kirk Boyer <kirk.boyer@webfilings.com>
2014-09-05 11:41:54 -0600

    put reporting stuff in its own module/file

--- console/pages.py
+++ console/pages.py
@@ -2,7 +2,6 @@
 import jinja2
 import os
 import webapp2
-import heapq
 
 # third-party modules
 from github import Github
@@ -10,6 +9,7 @@ from github import Github
 # project modules
 import models
 import settings
+from reporting import make_reports_available
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -19,72 +19,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 GIT_OBJECT = Github(login_or_token=settings.AUTH_TOKEN)
-
-
-def _mention_count_by_language(comments, languages):
-    num_mentions = {language: {} for language in languages}
-
-    for comment in comments:
-        for user in comment.users:
-            if user in num_mentions[comment.language]:
-                num_mentions[comment.language][user] += 1
-            else:
-                num_mentions[comment.language][user] = 1
-
-    return num_mentions
-
-
-def _top_users_by_language(num_mentions, languages, n_largest):
-    top_users = {}
-    for language in languages:
-        top_users[language] = heapq.nlargest(
-            n_largest,
-            num_mentions[language].iterkeys(),
-            key=(lambda user:
-                 num_mentions[language][user]))
-
-    return top_users
-
-
-def _language_stats_text(user, rank, mentions):
-    return "\t#{rank}: {user}, mentioned {mentions} times\n".format(
-        rank=rank, user=user, mentions=mentions)
-
-
-def _language_report_text(languages, num_mentions, n_largest):
-    report = "Top users by language, using # mentions in comments:\n"
-    top_users = _top_users_by_language(num_mentions, languages, n_largest)
-    for language in languages:
-        if language == 'None':  # for this report, only repos with languages.
-            continue
-
-        report += "Top users for {0}\n".format(language)
-
-        for rank, user in enumerate(top_users[language]):
-            report += _language_stats_text(
-                user, rank+1, num_mentions[language][user])
-    return report
-
-
-def _make_language_report_available(comments, languages, n_largest):
-    languages = set([comment.language for comment in comments])
-    num_mentions = _mention_count_by_language(comments, languages)
-    language_report = models.Report.get_or_insert('language_report')
-    language_report.text = _language_report_text(languages,
-                                                 num_mentions,
-                                                 n_largest)
-    language_report.put()
-
-
-def _make_connectivity_report_available(comments, languages, n_largest):
-    pass  # TODO: make this do something
-
-
-def _make_reports_available(n_largest=10, pr_body=True):
-    comments = models.Comment.query().fetch()
-
-    _make_language_report_available(comments, n_largest)
-    _make_connectivity_report_available(comments, n_largest)
 
 
 def _start_download(handler, file_content, filename):
@@ -113,13 +47,13 @@ class DownloadPage(webapp2.RequestHandler):
 
 class PrepareReportPage(webapp2.RequestHandler):
     def post(self):
-        _make_reports_available()
+        make_reports_available()
 
 
 class ReportPage(Page):
     def get(self):
         template_values = {}
-        _make_reports_available()
+        make_reports_available()
         self._make_page('report', template_values)
         self.redirect(settings.URLS['download'])
 
---
+++ console/reporting.py
@@ -0,0 +1,65 @@
+# built-in modules
+import heapq
+
+# project modules
+import models
+
+
+def _mentions_by_language(comments, languages):
+    num_mentions = {language: {} for language in languages}
+
+    for comment in comments:
+        for user in comment.users:
+            if user in num_mentions[comment.language]:
+                num_mentions[comment.language][user] += 1
+            else:
+                num_mentions[comment.language][user] = 1
+
+    return num_mentions
+
+
+def _top_users_by_language(num_mentions, languages, n_largest):
+    top_users = {}
+    for language in languages:
+        top_users[language] = heapq.nlargest(
+            n_largest,
+            num_mentions[language].iterkeys(),
+            key=(lambda user:
+                 num_mentions[language][user]))
+
+    return top_users
+
+
+def _language_stats_text(user, rank, mentions):
+    return "\t#{rank}: {user}, mentioned {mentions} times\n".format(
+        rank=rank, user=user, mentions=mentions)
+
+
+def _language_report_text(comments, n_largest):
+    languages = set([comment.language for comment in comments])
+    num_mentions = _mentions_by_language(comments, languages)
+    top_users = _top_users_by_language(num_mentions, languages, n_largest)
+    report = "Top users by language, using # mentions in comments:\n"
+    for language in languages:
+        if language == 'None':  # for this report, only repos with languages.
+            continue
+        report += "Top users for {0}\n".format(languages)
+        for rank, user in enumerate(top_users[language]):
+            report += _language_stats_text(
+                user, rank+1, num_mentions[language][user])
+    return report
+
+
+def _make_language_report_available(comments, n_largest):
+    language_report = models.Report.get_or_insert('language_report')
+    language_report.text = _language_report_text(n_largest)
+    language_report.put()
+
+
+def make_reports_available(n_largest=10, pr_body=True):
+    comments = models.Comment.query().fetch()
+    _make_language_report_available(comments, n_largest)
+
+
+def _make_connectivity_report_available(comments, n_largest):
+    pass
